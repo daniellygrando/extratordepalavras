ec3b1a8003094f2629ad9ce5d09767569cfaf3d8
daniellygrando <danielly.grando.silva@escola.pr.gov.br>
2025-07-03 11:03:09 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const botaoMostraPalavras = document.querySelector('#botaopalavrachave');
+
+botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
+
+
+function mostraPalavrasChave() { 
+    alert("fui clicado!"); 
+  } 
